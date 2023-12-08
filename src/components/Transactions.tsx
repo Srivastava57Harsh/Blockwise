69f86e0831f604127a845e054f87d10b81eb4d9c
thelordzeus <arnav55125@gmail.com>
2023-12-08 22:38:24 +0530

    Update font style in Transactions component

--- src/components/Transactions.tsx
+++ src/components/Transactions.tsx
@@ -8,7 +8,7 @@ export const Transactions = () => {
           <div className="rounded-t mb-0 px-4 py-3 border-0">
             <div className="flex flex-wrap items-center">
               <div className="relative w-full  max-w-full flex items-center">
-                <h3 className="font-semibold text-base text-blueGray-700">
+                <h3 className="font-semibold text-base text-blueGray-700 font-mono">
                   Transaction
                 </h3>
               </div>
@@ -19,10 +19,10 @@ export const Transactions = () => {
             <table className="items-center bg-transparent w-full border-collapse ">
               <thead>
                 <tr>
-                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold font-mono text-left">
                     Wallet Name
                   </th>
-                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left font-mono">
                     Amount
                   </th>
                 </tr>
