1a506b0d584ba089214b9ca53da28fa36bc972e2
thelordzeus <arnav55125@gmail.com>
2023-12-09 01:04:25 +0530

    Refactor wallets page layout and add wallet components

--- src/pages/wallets.tsx
+++ src/pages/wallets.tsx
@@ -1,10 +1,30 @@
+import { CiWallet } from "react-icons/ci";
+
 export default function Wallets() {
   return (
-    <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8 mt-10 m-8">
-      <div className="h-32 rounded-lg bg-gray-200">Wallet 1</div>
-      <div className="h-32 rounded-lg bg-gray-200">Wallet 2</div>
-      <div className="h-32 rounded-lg bg-gray-200">Wallet 3</div>
-      <div className="h-32 rounded-lg bg-gray-200">Wallet 4</div>
+    <div className=" mt-32 w-[90%] md:w-4/5 mx-auto h-[calc(100vh-72px)]">
+      <div className="rounded-lg bg-gray-200 text-black flex flex-col justify-evenly">
+        <div className="bg-white text-black font-bold py-2 px-4 rounded m-3 flex flex-col justify-between items-center mt-3 sm:flex-row space-y-3 p-4">
+          <div className="flex gap-2 items-center">
+            <CiWallet className="w-9 h-9" />
+            <span className="text-black">Savings Wallet</span>
+          </div>
+
+          <div className="bg-black rounded-lg p-2">
+            <button className="text-white font-mono font-thin">20 ETH</button>
+          </div>
+        </div>
+        <div className="bg-white text-black font-bold py-2 px-4 rounded m-3 flex flex-col justify-between items-center mt-3 sm:flex-row space-y-3 p-4">
+          <div className="flex gap-2 items-center">
+            <CiWallet className="w-9 h-9" />
+            <span className="text-black">Trading Wallet</span>
+          </div>
+
+          <div className="bg-black rounded-lg p-2">
+            <div className="text-white font-mono font-thin">20 ETH</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
