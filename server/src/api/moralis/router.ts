47161a98da3f92576e0bfc601d0e4c486cc9a9a9
Srivastava57Harsh <srivastava57harsh@gmail.com>
2023-12-09 00:27:00 +0530

    :bug: Fix moralis router
    fix moralis router

--- server/src/api/moralis/router.ts
+++ server/src/api/moralis/router.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from 'express';
 import LoggerInstance from '../../loaders/logger';
+import { ethers } from 'ethers';
+
 // import { fetchTransaction } from './controller';
 import Moralis from 'moralis';
 import { EvmChain } from '@moralisweb3/common-evm-utils';
@@ -7,56 +9,34 @@ import { EvmChain } from '@moralisweb3/common-evm-utils';
 const moralisRouter = Router();
 
 async function handleTransactions(req: Request, res: Response) {
-  // const walletAddress = req.headers['wallet-address'];
-  // const result = await fetchTransaction(walletAddress);
-
-  const walletAddress = req.headers['wallet-address']; // Modify this according to your actual header name
+  const walletAddress = req.headers['wallet-address'];
 
   if (req.method !== 'GET') {
     return res.status(405).end(); // Method Not Allowed
   }
 
   try {
-    Moralis.start({
-      appId: 'keyjbaksadhbashd',
+    await Moralis.start({
+      apiKey: '85yl2Ke8rQsciJKsNglvOq9aBFIsRYADqMV9KcJ0QjWGOyaeH3uxcRQvhtdKxgGr',
     });
-
-    // Use the extracted wallet address
-    const address = '0x26fcbd3afebbe28d0a8684f790c48368d21665b5';
-    // const address = walletAddress || req.query.address;
-    const chain = EvmChain.ETHEREUM; // Update with the correct chain if needed
-
-    // Validate that the address is provided
-    if (!address) {
-      return res.status(400).json({ error: 'Wallet address is required' });
-    }
 
     const response = await Moralis.EvmApi.transaction.getWalletTransactions({
-      //@ts-ignore
-      address,
-      chain,
+      chain: EvmChain.ETHEREUM,
+      address: (walletAddress as string) || '0x8EA809076374708aEF0d6e9C3F0a7A64CAD17368',
     });
 
-    console.log('harsh', response);
-
-    if (response) {
-      res.status(201).json({
-        message: 'Success',
-        data: response.toJSON,
-      });
-    } else {
-      throw {
-        status: 400,
-        // message: result.message,
-      };
-    }
+    console.log(response.raw);
+    res.send(response.raw);
   } catch (e) {
+    console.error(e);
+
     LoggerInstance.error(e);
     res.status(e.status || 500).json({
       message: e.message || 'Request Failed',
     });
   }
 }
+
 moralisRouter.get('/transactions', handleTransactions);
 
 export default moralisRouter;
