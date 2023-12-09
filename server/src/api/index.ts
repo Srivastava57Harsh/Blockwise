eb8c04523cb99ba5e4d0261f59d49d93c5e3d75a
Srivastava57Harsh <srivastava57harsh@gmail.com>
2023-12-09 06:27:15 +0530

    :ambulance: Fixes
    fixes

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 // import razorpayRouter from './razorpay/router';
 import userRouter from './user/router';
 import moralisRouter from './moralis/router';
+import razorpayRouter from './razorpay/router';
 
 export default (): Router => {
   const app = Router();
 
   app.use('/user', userRouter);
   app.use('/moralis', moralisRouter);
+  app.use('/razorpay', razorpayRouter);
 
   return app;
 };
--- server/src/api/razorpay/model.ts
+++
@@ -1,9 +0,0 @@
-// interface OrderDetails {
-//   email: string;
-//   courseId: string;
-//   generateOrderId?: string;
-//   slug?: {};
-//   isPaid?: boolean;
-// }
-
-// export default OrderDetails;
